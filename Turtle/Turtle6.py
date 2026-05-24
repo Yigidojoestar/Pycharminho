09eb2dba094ebf28e38aa82a813928868cc4ed91
Yiğithan Hayta <yigithan_5206@hotmail.com>
2026-05-24 20:23:50 +0300

    Çember çizim

---
+++ Turtle/Turtle6.py
@@ -0,0 +1,16 @@
+import turtle
+
+#Çember Çizimi
+
+annnd=turtle.Screen()
+annnd.bgcolor("Light Blue")
+annnd.title("Çember")
+
+çembo=turtle.Turtle()
+çembo.pencolor("Black")
+çembo.pensize(10)
+
+çembo.circle(150)
+
+turtle.done()
+
