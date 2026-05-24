435d8f6c7bf9fab6a9b1299a903bf22f7ce5c0db
Yiğithan Hayta <yigithan_5206@hotmail.com>
2026-05-24 19:38:09 +0300

    Guts işareti

---
+++ Turtle/Turtle1.py
@@ -0,0 +1,55 @@
+import turtle
+
+#Berserk Guts'ın Berserker işareti :D
+
+drawing_board = turtle.Screen()
+drawing_board.bgcolor("Red")
+
+turtle_instance = turtle.Turtle()
+turtle_instance.forward(100)
+turtle_instance.left(90)
+turtle_instance.forward(75)
+
+vışş=turtle.Turtle()
+vışş.left(90)
+vışş.forward(100)
+vışş.right(90)
+vışş.forward(75)
+
+turtleee=turtle.Turtle()
+turtleee.back(100)
+turtleee.left(90)
+turtleee.back(100)
+turtleee.left(90)
+turtleee.back(100)
+turtleee.left(90)
+turtleee.back(100)
+turtleee.left(90)
+turtleee.back(100)
+
+ahaha=turtle.Turtle()
+ahaha.left(45)
+ahaha.forward(143)
+
+yuh=turtle.Turtle()
+yuh.left(45)
+yuh.forward(143)
+yuh.left(35)
+yuh.forward(20)
+
+ana=turtle.Turtle()
+ana.left(45)
+ana.forward(143)
+ana.forward(30)
+
+pp=turtle.Turtle()
+pp.left(45)
+pp.forward(143)
+pp.right(35)
+pp.forward(25)
+
+hhaa=turtle.Turtle()
+hhaa.right(135)
+hhaa.forward(143)
+
+turtle.done()
