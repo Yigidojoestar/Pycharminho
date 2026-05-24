b45aa560fad825295d992717d3cb7d3b26c47d57
Yiğithan Hayta <yigithan_5206@hotmail.com>
2026-05-24 19:56:29 +0300

    Joestar star işareti

---
+++ Turtle/Turtle3.py
@@ -0,0 +1,20 @@
+import turtle
+
+#Yıldız işareti(Jojo referans)
+
+çizme_tahtası=turtle.Screen()
+çizme_tahtası.bgcolor("Purple")
+
+star=turtle.Turtle()
+star.right(144)
+star.forward(300)
+star.right(144)
+star.forward(300)
+star.right(144)
+star.forward(300)
+star.right(144)
+star.forward(300)
+star.right(144)
+star.forward(300)
+
+turtle.done()
